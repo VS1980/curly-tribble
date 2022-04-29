d8026110bc5f31d1ae98e2ecaab953daeaebf2a1
VS1980 <vaibhavsalaskar@outlook.com>
2022-04-29 19:46:16 -0400

    Create Beragfs.php

---
+++ Beragfs.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Berafgs</title>
+</head>
+<h1>Berafgs</h1>
+<br>
+<body class="body">
+	<hr>
+	<hr>
+<h2>THis is a fake website made for entertainment</h2>
+	
+<style>
+	.body{
+		font-family: monospace;
+		background-image: url(https://images.pexels.com/photos/2770933/pexels-photo-2770933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+		font-weight: bolder;
+		color: ghostwhite;
+	}
+	
+</style>
+</html>
